98e35223ae68b8fddaa59c059f7f9022fc3cb4d0
m3hu1 <pathakkmehul@gmail.com>
2024-04-11 18:29:02 +0530

    updated services.tsx

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,30 +1,52 @@
-import Link from "next/link";
+// import Link from "next/link";
 
+
+// const Services = () => {
+//   return (
+//     <footer className="text-white-800 flex-between body-text w-full gap-y-10 border-t border-black-400 bg-black-100 px-20 py-12 max-md:flex-col glow-effect" style={{ position: 'relative', zIndex: 1 }}>
+//       <div className="flex justify-between gap-x-9">
+//         <Link href="/redirect" className="hidden sm:inline">
+//           Redirect Tracker
+//         </Link>
+//         <Link href="/iprep" className="hidden sm:inline">
+//             IP Rep
+//         </Link>
+//         {/* <Link href="/privacypolicy" className="hidden sm:inline">
+//           Latest CyberSec News
+//         </Link> */}
+//         {/* <Link href="/viruscheck" className="hidden sm:inline">
+//           Malicious File Checker
+//         </Link> */}
+//         <Link href="/sslcheck" className="hidden sm:inline">
+//           SSL Certificate
+//         </Link>
+//         {/* <Link href="/privacypolicy" className="hidden sm:inline">
+//           Python Package Checker
+//         </Link> */}
+//         {/* <Link href="/privacypolicy" className="hidden sm:inline">
+//           CyberSec Norms
+//         </Link> */}
+//       </div>
+//     </footer>
+//   );
+// };
+
+// export default Services;
+
+import Link from "next/link";
 
 const Services = () => {
   return (
-    <footer className="text-white-800 flex-between body-text w-full gap-y-10 border-t border-black-400 bg-black-100 px-20 py-12 max-md:flex-col glow-effect" style={{ position: 'relative', zIndex: 1 }}>
-      <div className="flex gap-x-9">
-        <Link href="/redirect" className="hidden sm:inline">
+    <footer className="text-white-800 body-text w-full border-t border-black-400 bg-black-100 px-20 py-12">
+      <div className="flex justify-between">
+        <Link href="/redirect" className="block">
           Redirect Tracker
         </Link>
-        <Link href="/iprep" className="hidden sm:inline">
-            IP Rep
+        <Link href="/iprep" className="block">
+          IP Reputation Checker
         </Link>
-        <Link href="/privacypolicy" className="hidden sm:inline">
-          Latest CyberSec News
-        </Link>
-        <Link href="/viruscheck" className="hidden sm:inline">
-          Malicious File Checker
-        </Link>
-        <Link href="/sslcheck" className="hidden sm:inline">
-          SSL Certificate
-        </Link>
-        <Link href="/privacypolicy" className="hidden sm:inline">
-          Python Package Checker
-        </Link>
-        <Link href="/privacypolicy" className="hidden sm:inline">
-          CyberSec Norms
+        <Link href="/sslcheck" className="block">
+          SSL Certificate Checker
         </Link>
       </div>
     </footer>
