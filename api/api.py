cc6eebe76a1517778a97b4ea4db820a5f7bbf4fd
m3hu1 <pathakkmehul@gmail.com>
2024-04-11 16:07:46 +0530

    updated api

--- api/api.py
+++ api/api.py
@@ -1,4 +1,4 @@
-#/api/api.py
+# /api/api.py
 from fastapi import FastAPI, Header
 from fastapi.middleware.cors import CORSMiddleware
 import requests
@@ -35,13 +35,46 @@ def check_redirects(url: str):
 @app.get("/check-ip-reputation")
 def check_ip_reputation(ip: str, x_key: str = Header(None)):
     try:
-        if x_key != "44537bf504d736ece21c79b60c23a8bb2254d1508e255501d29139194b0fd4774074afed10c5192e":
-            return {"error": "Invalid API key"}
-        
-        response = requests.get(f"https://api.abuseipdb.com/api/v2/check?ipAddress={ip}", 
+        response = requests.get(f"https://redirect-tracker-api.onrender.com/check-ip-reputation?ip=${ip}", 
                                 headers={"Key": "44537bf504d736ece21c79b60c23a8bb2254d1508e255501d29139194b0fd4774074afed10c5192e", 
                                          "Accept": "application/json"})
         data = response.json()
         return data
+    except Exception as e:
+        return {"error": str(e)}
+
+@app.get("/check-ssl")
+def check_ssl(url: str):
+    try:
+        headers = {
+            "X-RapidAPI-Key": "876ec4f793msh46e01bc9724cf74p127786jsn2d01e3fe7ae0",
+            "X-RapidAPI-Host": "ssl-certificate-checker2.p.rapidapi.com"
+        }
+
+        querystring = {"host": url}
+
+        response = requests.get("https://redirect-tracker-api.onrender.com/check-ssl?url=${url}",
+                                headers=headers, params=querystring)
+
+        data = response.json()
+
+        cert_info = {
+            "subject": data["subject"]["CN"],
+            "issuer": data["issuer"]["CN"],
+            "valid_from": data["validFrom"],
+            "valid_until": data["validTo"],
+            "expires_in_days": data["expiresInDays"],
+            "fingerprint": data["fingerprint"],
+            "fingerprint256": data["fingerprint256"],
+            "serial_number": data["serialNumber"],
+            "pem": data["pem"],
+            "protocol": data["protocol"],
+            "cipher_name": data["cipher"]["name"],
+            "cipher_standard_name": data["cipher"]["standardName"],
+            "cipher_version": data["cipher"]["version"],
+            "ocsp_uri": data["infoAccess"]["OCSP - URI"],
+            "ca_issuers_uri": data["infoAccess"]["CA Issuers - URI"]
+        }
+        return cert_info
     except Exception as e:
         return {"error": str(e)}
