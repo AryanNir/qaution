4c30d004c64dc3db9224f56d75b52ba8ab6dc3c9
m3hu1 <pathakkmehul@gmail.com>
2024-04-29 00:10:23 +0530

    fixed about us page

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -22,7 +22,7 @@ const features = [
 
 const aboutus = () => {
   return (
-    <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
+    <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col -mt-40">
       <section className="nav-padding w-full">
         <div className="flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-cover bg-center text-center">
           <h1 className="smXheading1 heading2 mb-6 text-center text-gradient_blue-purple">
