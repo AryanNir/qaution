d8bb86d3116c3e2f174e3ea68f4a78d3b6d4db7a
m3hu1 <pathakkmehul@gmail.com>
2024-04-28 20:44:16 +0530

    highlighted the services

--- components/Services.tsx
+++ components/Services.tsx
@@ -38,8 +38,8 @@ import Link from "next/link";
 const Services = () => {
   return (
     <footer className="text-white-800 body-text w-full border-t border-black-400 bg-black-100 px-20 py-12">
-      <div className="flex justify-between">
-        <Link href="/redirect" className="block">
+      <div className="flex justify-between underline">
+        <Link href="/redirect" className="block ">
           Redirect Tracker
         </Link>
         <Link href="/iprep" className="block">
